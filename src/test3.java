bc5db11ec62cb4563b5cccc37f6ffb7c0b718009
hahayoungyou <whgkdud0501@naver.com>
2022-05-05 23:23:09 +0900

    수정

--- src/test3.java
+++ src/test3.java
@@ -15,6 +15,7 @@ public class test3 {
             }
                 sum = Math.round( sum/10);
             System.out.format("#%d %d\n", tc,(int)sum); //평균을 구하는 것은 올림 반올림 내림 확인
+            // system.out.format 기본적으로는 오른쪽 정렬, 왼쪽정렬은 - 기호
             //Math.floor() : 소수점 이하를 버림한다.
             //Math.ceil() : 소수점 이하를 올림한다.
             //Math.round() : 소수점 이하를 반올림한다
