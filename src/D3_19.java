56c7a86b3b9321350f561fc5d9d21ef49bbfb3a1
hahayoungyou <whgkdud0501@naver.com>
2022-05-15 01:00:08 +0900

    D3_2,4,7,11,17다시 풀기 ㅎㅅㅇㄷ 88-2-수정

--- src/D3_19.java
+++ src/D3_19.java
@@ -2,7 +2,46 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-10912. 외로운 문자
+//10912. 외로운 문자
+
+//이걸 QUEUE를 활용할 수 있다. 자료구조 공부해야할듯.
+/*import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Solution {
+    static PriorityQueue queue = new PriorityQueue();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int loop = 0; loop < T; loop++) {
+            String input = br.readLine();
+            StringBuilder sb = new StringBuilder();
+            queue.clear();
+            for (int i = 0; i < input.length(); i++) {
+                char letter = input.charAt(i);
+                if (queue.contains(letter)){
+                    queue.remove(letter);
+                }else{
+                    queue.offer(letter);
+                }
+            }
+            if (queue.isEmpty()){
+                sb.append("Good");
+            }else{
+                while(!queue.isEmpty()){
+                    sb.append(queue.poll());
+                }
+            }
+
+            System.out.println("#" + (loop+1) + " " + sb);
+        }
+
+
+        br.close();
+    }
+}*/
 public class D3_19 {
     public static void main(String[] args) {
 
