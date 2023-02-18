254c8d04f80b2d4caba3cdc5cc6a719b6c52f534
johayeong <whgkdud0501@naver.com>
2023-02-18 16:37:12 +0900

    SAMSUNG SW 아카데미 7829. 보물왕 태혁

--- src/baekjoon_53.java
+++ src/baekjoon_53.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Stack;
 
-//쇠막대기
+//SAMSUNG SW 아카데미 7829. 보물왕 태혁
 public class baekjoon_53 {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        for( int i=1;i<=a ;i++){
+            int b=sc.nextInt();
+            ArrayList<Integer> arr= new ArrayList<>();
+            for(int j=0;j<b;j++){
+                arr.add(sc.nextInt());
+            }
 
-
-
-
-
-
-
+            Collections.sort(arr);
+            System.out.println("#"+i+" "+arr.get(0)*arr.get(b-1));
+        }
     }
 }
